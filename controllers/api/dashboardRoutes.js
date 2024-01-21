efad176ff82bf79f7c104396f63e778e08208170
davidmelendez <Dmelz11@github.com>
2024-01-21 12:43:05 -0800

    added routing to get, and edit posts and comments in dashboard if logged in

--- controllers/api/dashboardRoutes.js
+++ controllers/api/dashboardRoutes.js
@@ -1,35 +1,78 @@
 // imports
 const router = require("express").Router();
-const { Post, User } = require("../../models");
+const sequelize = require('../config/connection');
+const { Post, Comment, User } = require("../../models");
 const withAuth = require("../utils/auth");
 
 // READ all posts
 router.get("/",withAuth, async (req, res) => {
   try {
-    const postData = await Post.findAll({
+    const postData = await Comment.findAll({
+        where: {user_id: req.session.user_id},
+        attributes: ['id', 'title','body'],
       include: [
         {
-          model: User,
-          attributes: ["id:","username"],
-        },
-      ],
-      order:[["createdAt","DESC"]],
+          model: Comment,
+          attributes: ["id","comment_text","user_id"],
+
+       include: {
+         model: User, 
+         attributes: ["username"]},
+       },
+       { model: User,
+         attributes:["username"]},
+       ],
     });
-
-    res.status(200).json(postData);
-  } catch (err) {
-    console.error(err);
-    res.status(500).json(err);
-  }
-});
-
-const posts =postData.map((post)=>
+    const posts =postData.map((post)=>
 post.get({plain:true}));
 res.render("dashboard",{
     loggedIn: req.session.loggedIn,
     loggedInUserData: req.session.loggedInUserData,
-    posts: posts,
+    posts: posts, 
+})
+} catch (err) {
+    console.error(err);
+    res.status(500).json(err);
+  }
 });
-// UPDATE Comment
+router.get("/edit/:id",withAuth, async (req, res) => {
+    try {
+      const postData = await Comment.findOne({
+          where: {user_id: req.session.user_id},
+          attributes: ['id', 'title','body'],
+        include: [
+          {
+            model: Comment,
+            attributes: ["id","comment_text","user_id"],
+  
+         include: {
+           model: User, 
+           attributes: ["username"]},
+         },
+         { model: User,
+           attributes:["username"]},
+         ],
+      });
+      if(!postData){
+        res.status(404).json(err);
+        return;
+      }
+      postDataNew = postData.get({plain: true});
+      res.render("edit-post",{
+        loggedIn: true,
+      });
+  } catch (err) {
+      console.error(err);
+      res.status(500).json(err);
+    }
+  });
+  router.get("/new",(req, res)=>{
+    res.render("new-post",{
+        layout: "dashboard",
+    });
+  });
+
+  module.exports=router;
+
 
 
