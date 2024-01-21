6ce3f1cbb1d136c78c02e0270611537cfa857c1f
davidmelendez <Dmelz11@github.com>
2024-01-21 09:04:47 -0800

    added logical relationship association between users, posts and comments noting foreignKeys  in index.js file in models

---
+++ models/index.js
@@ -0,0 +1,31 @@
+const User = require('./Users');
+const Post = require('./Post');
+const Comment = require('./Comment');
+
+
+User.hasMany(Post, {
+  foreignKey: 'user_id',
+  onDelete: 'CASCADE'
+});
+
+Post.belongsTo(User, {
+  foreignKey: 'user_id',
+  onDelete: 'CASCADE'
+});
+
+Post.hasMany(Comment, {
+  foreignKey: '_id',
+  onDelete: 'CASCADE'
+});
+
+Comment.belongsTo(User, {
+  foreignKey: 'user_id',
+  onDelete: 'CASCADE'
+});
+
+Comment.belongsTo(Post, {
+  foreignKey: 'techdata_id',
+  onDelete: 'CASCADE'
+});
+
+module.exports = { User, Post, Comment };
