0c93f85bc93989ee476b29db7824f4dcc6eb87d1
davidmelendez <Dmelz11@github.com>
2024-01-22 09:43:29 -0800

    create postHandler

---
+++ public/js/create-post.js
@@ -0,0 +1,46 @@
+async function createPostHandler(event) {
+    event.preventDefault();
+    
+    const title = document.querySelector("#new-post-title").value.trim();
+    const body = document.querySelector("#new-post-body").value.trim();
+    console.log(title, body)
+
+    if (title && body) {
+      //aquiring posts text with title
+      const response = await fetch("/api/posts", {
+        method: "POST",
+        body: JSON.stringify({
+          title,
+          body,
+        }),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+  
+      //checking if good
+      if (response.ok) {
+        localStorage.setItem("post", JSON.stringify({postTitle: title, postBody: body}));
+        var post = JSON.parse(localStorage.getItem("post"));
+        console.log("Test");
+ 
+        renderMessage();
+        
+      } else {
+        alert(response.statusText); // find better way to do this
+    }
+  }
+}
+// listening for create post button click
+document
+  .querySelector(".create-post-btn")
+  .addEventListener("click", createPostHandler);
+
+
+function renderMessage() {
+  var lastpost = JSON.parse(localStorage.getItem("post"));
+  console.log(lastpost);
+  if (lastpost !== null) {
+    document.location.replace("/dashboard");
+  }
+}
