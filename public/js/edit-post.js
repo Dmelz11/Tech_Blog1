6ab1cb911ba7c8de80055604cad85c668e4c5c36
davidmelendez <Dmelz11@github.com>
2024-01-22 10:10:16 -0800

    working on creating edit button

---
+++ public/js/edit-post.js
@@ -0,0 +1,15 @@
+let post = window.location.pathname.split("/");
+
+// Function created allowing user to edit blog posts from the individual blog post page
+const editPost = async (event) => {
+  event.preventDefault();
+  console.log("clicked me");
+// get text and trim whitespace
+const comment_body = document.getElementById("#editBtn").value.trim();
+
+console.log(blogPost);
+
+document.location.assign(`/createpost/${blogPost[2]}`);
+};
+
+const editButton = document.querySelectorAll("#editBtn");
