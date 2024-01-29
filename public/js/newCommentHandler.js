2d40a7bc1c7b9bd90659fdfdc4c32a4c945f80d6
davidmelendez <Dmelz11@github.com>
2024-01-29 14:32:57 -0800

    changing post variable to blog post

--- public/js/newCommentHandler.js
+++ public/js/newCommentHandler.js
@@ -8,18 +8,20 @@ async function newCommentHandler(event) {
     const comment_body = document.getElementById("comment").value.trim();
     // get post id from URL
     const url = window.location.toString().split("/");
-    const post_id = url[url.length - 1];
+    const blogPost_id = url[url.length - 1];
+
     if (comment_body) {
         const response = await fetch("/api/comment", {
           method: "POST",
           body: JSON.stringify({
-            post_id,
+            blogPost_id,
             comment_body,
           }),
           headers: {
             "Content-Type": "application/json",
           },
         });
+        
         if (response.ok) {
             document.location.reload();
           } else {
