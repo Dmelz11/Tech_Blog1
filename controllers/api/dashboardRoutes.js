112595d28fa86658791a15da5f0a86c7bbef3f16
davidmelendez <Dmelz11@github.com>
2024-01-29 14:17:48 -0800

    trying to debug as app wont load so checking syntax

--- controllers/api/dashboardRoutes.js
+++ controllers/api/dashboardRoutes.js
@@ -1,81 +1,76 @@
-// imports
+//imports
 const router = require("express").Router();
-const sequelize = require('../config/connection');
+const sequelize = require("../config/connection");
 const { Post, Comment, User } = require("../../models");
 const withAuth = require("../utils/auth");
 
 // READ all posts in dashboard only if logged in
-router.get("/",withAuth, async (req, res) => {
+router.get("/", withAuth, async (req, res) => {
   try {
     const postData = await Comment.findAll({
-        where: {user_id: req.session.user_id},
-        attributes: ['id', 'title','body'],
+      where: { user_id: req.session.user_id },
+      attributes: ["id", "title", "body"],
       include: [
         {
           model: Comment,
-          attributes: ["id","comment_body","user_id"],
+          attributes: ["id", "comment_body", "user_id"],
 
-       include: {
-         model: User, 
-         attributes: ["username"]},
-       },
-       { model: User,
-         attributes:["username"]},
-       ],
+          include: {
+            model: User,
+            attributes: ["username"],
+          },
+        },
+        { model: User, attributes: ["username"] },
+      ],
     });
     //serialization
-    const posts =postData.map((post)=>
-post.get({plain:true}));
-res.render("dashboard",{
-    loggedIn: req.session.loggedIn,
-    loggedInUserData: req.session.loggedInUserData,
-    posts: posts, 
-})
-} catch (err) {
+    const posts = postData.map((post) => post.get({ plain: true }));
+    res.render("dashboard", {
+      loggedIn: req.session.loggedIn,
+      loggedInUserData: req.session.loggedInUserData,
+      posts: posts,
+    });
+  } catch (err) {
     console.error(err);
     res.status(500).json(err);
   }
 });
 //routing to edit an existing post
-router.get("/edit/:id",withAuth, async (req, res) => {
-    try {
-        
-      const postData = await Comment.findOne({
-          where: {user_id: req.session.user_id},
-          attributes: ['id', 'title','body'],
-        include: [
-          {
-            model: Comment,
-            attributes: ["id","comment_body","user_id"],
-  
-         include: {
-           model: User, 
-           attributes: ["username"]},
-         },
-         { model: User,
-           attributes:["username"]},
-         ],
-      });
-      if(!postData){
-        res.status(404).json({message:"Unable to locate post with this id"});
-        return;
-      }
-      postDataNew = postData.get({plain: true});
-      res.render("edit-post",{
-        loggedIn: true,
-      });
-  } catch (err) {
-      console.error(err);
-      res.status(500).json(err);
-    }
-  });
-  router.get("/new",(req, res)=>{
-    res.render("new-post",{
-        layout: "dashboard",
+router.get("/edit/:id", withAuth, async (req, res) => {
+  try {
+    const postData = await Comment.findOne({
+      where: { user_id: req.session.user_id },
+      attributes: ["id", "title", "body"],
+      include: [
+        {
+          model: Comment,
+          attributes: ["id", "comment_body", "user_id"],
+
+          include: {
+            model: User,
+            attributes: ["username"],
+          },
+        },
+        { model: User, attributes: ["username"] },
+      ],
     });
+    if (!postData) {
+      res.status(404).json({ message: "Unable to locate post with this id" });
+      return;
+    }
+    postDataNew = postData.get({ plain: true });
+    res.render("edit-post", {
+      loggedIn: true,
+    });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json(err);
+  }
+});
+router.get("/new", (req, res) => {
+  res.render("new-post", {
+    layout: "dashboard",
   });
+});
 
-  module.exports=router;
-
-
-
+module.exports = router;
